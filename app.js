ada7cd59c2050e428c354785b19442d0936c103c
machuanjia <machuanjia@worktile.com>
2017-09-25 10:34:55 +0800

    commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// /**
+//  * Created by yanshi0429 on 2017/9/22.
+//  */
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
+
+router.get('/', ctx => {
+    ctx.body = 'Hello Koa';
+});
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+const server = app.listen(3000, () => {
+        console.log(`server is started... 3000`);
+});
+
+module.exports = server;
